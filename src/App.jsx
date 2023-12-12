e2f29dcc77370070b7d028e5704d8e44acc6c91e
pela-87 <villarrueljonatana@gmail.com>
2023-12-11 21:14:05 -0300

    Se agrego otra columna en donde van a ir las tareas finaliazadas

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,18 @@ function App() {
       <div>
         <h1 className="text-2xl font-bold text-center">Organizador de Tareas</h1>
       </div>
-      <main className="max-w-[95%] w-[80rem] mx-auto grid lg:grid-cols-2 gap-8 my-10 relative">
-        <section className='w-full sticky'>
+      <main className="max-w-[80%] w-[80rem] grid lg:grid-cols-3 gap-2 my-8 relative">
+        <section className='max-w-[95%] w-[20rem] sticky'>
           <Formulario tarea={tarea} setTarea={setTarea} tareas={tareas} setTareas={setTareas} />
         </section>
-        <section className="w-full flex flex-col gap-3">
+        <section className="max-w-[80%] w-[20rem] flex flex-col gap-2">
+          <h2 className="text-xl font-bold text-blue-700 text-center">Tareas Pendientes</h2>
+          {tareas.map(c => {
+        return <Tarea key={c.id} tarea={c} setTarea={setTarea} tareas={tareas} setTareas={setTareas} />
+      })}
+        </section>
+        <section className="max-w-[80%] w-[20rem] flex flex-col gap-2">
+        <h2 className="text-xl font-bold text-red-600 text-center">Tareas Finalizadas</h2>
           {tareas.map(c => {
         return <Tarea key={c.id} tarea={c} setTarea={setTarea} tareas={tareas} setTareas={setTareas} />
       })}
